7b79161593c5dc53336acb3ddafc52980be1bc78
ABHIJIT649 <abhijitmohanty440@gmail.com>
2025-08-12 13:51:11 +0530

    add practice tailwind

---
+++ 03taliwindprops/src/App.jsx
@@ -0,0 +1,29 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './Card'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  let myObj = {
+    username : "abhijit",
+    age:21
+  }
+
+  let newArr = [1, 2 , 3]
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-5
+       rounded-2xl ' >Tailwind test</h1>
+
+<Card username ="chai or code" second = "click me" />
+<Card username= "Abhijit" second = "not click me"/>
+      
+    </>
+  )
+}
+
+export default App
